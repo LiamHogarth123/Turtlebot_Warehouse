362b11d9c3eca7267eb7528f5bed622dc0448212
john <john.e.hunter-1@student.uts.edu.au>
2024-03-04 16:50:05 +1100

    class_definitions

--- mapping_john/src/BoundaryDetection.h
+++ mapping_john/src/BoundaryDetection.h
@@ -1,1 +1,26 @@
+#ifndef BOUNDARYDETECTION_H
+#define BOUNDARYDETECTION_H
 #include "ros/ros.h"
+
+class BoundaryDetection
+{
+public:
+    /**
+     * @brief Constructor
+     */
+    BoundaryDetection();
+
+    /**
+     * @brief Destructor
+     */
+    ~BoundaryDetection();
+
+    /**
+     * @brief Detect boundary colour
+     * @param[in] image Image from webcam
+     * @return True if boundary detected, else false
+     */
+    bool detectColour(long image);
+};
+
+#endif
