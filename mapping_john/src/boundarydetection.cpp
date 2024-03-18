93cb6a922dedaa74f2c5a39c14d0ad5d54c64610
john <john.e.hunter-1@student.uts.edu.au>
2024-03-18 13:35:09 +1100

    boundarydetection_implementation

--- mapping_john/src/boundarydetection.cpp
+++ mapping_john/src/boundarydetection.cpp
@@ -2,8 +2,6 @@
 #include <math.h>
 #include <vector>
 #include <iostream>
-#include <chrono>
-#include <thread>
 
 // #include <opencv2/highgui/highgui.hpp>
 // #include <opencv2/highgui.hpp>
@@ -18,14 +16,48 @@ BoundaryDetection::~BoundaryDetection()
 
 }
 
-// bool BoundaryDetection::detectColour(long image)
-bool BoundaryDetection::detectColour()
+bool BoundaryDetection::openCVtest()
 {
     cv::Mat input = cv::imread("/home/john/Desktop/image.jpg");
     cv::imshow("Window",input);
     cv::waitKey(0);
 
-    // std::this_thread::sleep_for(std::chrono::nanoseconds(1000000));
-
     return 0;
+}
+
+double BoundaryDetection::detectColour(cv::Mat image)
+{
+    // Define variables
+    double boundaryFlag = 0; // Default to no boundary
+    double y; // Image 'y' coordinate
+    double x; // Image 'x' coordinate
+    cv::Size imageSize = image.size();
+    cv::Point3_<uchar>* pixelRGB = image.ptr<cv::Point3_<uchar>>(y,x);
+
+    /* Where
+    pixelRGB.x = blue
+    pixelRGB.y = green
+    pixelRGB.z = red
+    */
+
+    // Iterate through each pixel, top left to bottom right
+    for (int i = 0; i < imageSize.height; i++)
+    {
+        for (int j = 0; i < imageSize.width; i++)
+        {
+            // Check if blue intensity is above threshold
+            if (pixelRGB->x > blue_threshold_)
+            {
+                return boundaryFlag = 1;
+            }
+
+            // Check if red intensity is above threshold
+            if (pixelRGB->z > red_threshold_)
+            {
+                return boundaryFlag = 2;
+            }
+        }
+    }
+    
+    return boundaryFlag;
 }
