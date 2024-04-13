bcc9548717d1d71c277ccc80a01d66249e3af399
danchen02 <126075501+danchen02@users.noreply.github.com>
2024-04-13 13:30:53 +1000

    Delete control_Dan/build directory

--- control_Dan/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/john/Desktop/Turtlebot_Warehouse/control_Dan
-# Build directory: /home/john/Desktop/Turtlebot_Warehouse/control_Dan/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
