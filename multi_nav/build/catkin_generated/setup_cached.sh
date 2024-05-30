1ac61378dc78d852edfb7101fb59b1ba47ae9a1e
john <john.e.hunter-1@student.uts.edu.au>
2024-05-31 08:18:16 +1000

    multi_nav_updated

--- multi_nav/build/catkin_generated/setup_cached.sh
+++ multi_nav/build/catkin_generated/setup_cached.sh
@@ -8,5 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/john/catkin_ws/src/Turtlebot_Warehouse/multi_nav/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/john/catkin_ws/src/Turtlebot_Warehouse/multi_nav/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/john/catkin_ws/src/Turtlebot_Warehouse/multi_nav/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/john/catkin_ws/src/Turtlebot_Warehouse/multi_nav/build'
 export ROS_PACKAGE_PATH="/home/john/catkin_ws/src/Turtlebot_Warehouse/multi_nav:$ROS_PACKAGE_PATH"
