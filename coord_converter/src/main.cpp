52213fba60fb71063a738d2b6d1cf0fa88cf75d2
john <john.e.hunter-1@student.uts.edu.au>
2024-05-30 11:57:12 +1000

    converter

--- coord_converter/src/main.cpp
+++ coord_converter/src/main.cpp
@@ -2,11 +2,6 @@
 #include "ros/ros.h"
 #include <iostream>
 
-struct Quaternion
-{
-    double x, y, z, w;
-};
-
 /** Function to convert a quaternion to a yaw value*/
 double quaternionToYaw(double x, double y, double z, double w)
 {
@@ -17,16 +12,13 @@ double quaternionToYaw(double x, double y, double z, double w)
 }
 
 /** Function to convert a yaw value to a quaternion*/
-Quaternion yawToQuaternion(double yaw)
+std::tuple<double, double, double, double> yawToQuaternion(double yaw)
 {
-    Quaternion q;
-
-    q.x = 0.0;
-    q.y = 0.0;
-    q.z = std::sin(yaw / 2.0);
-    q.w = std::cos(yaw / 2.0);
-
-    return q;
+    double x = 0.0;
+    double y = 0.0;
+    double z = std::sin(yaw / 2.0);
+    double w = std::cos(yaw / 2.0);
+    return std::make_tuple(x, y, z, w);
 }
 
 int main(int argc, char **argv)
@@ -37,32 +29,89 @@ int main(int argc, char **argv)
     /** Create node handle*/
     ros::NodeHandle nh;
 
-    double x = 0.0;
-    double y = 0.0;
-    double z = 0.0;
-    double w = 1.0;
+    // /** Initialise process variable*/
+    // int process;
 
-    double yaw = quaternionToYaw(x, y, z, w);
-    std::cout << "Yaw: " << yaw << std::endl;
+    // /** Nominate process*/
+    // if (argc > 1)
+    // {
+    //     process = std::stod(argv[1]);
+    //     ROS_INFO("The 'process' argument is %d", process);
+    // }
+    // /** Otherwise warn*/
+    // else
+    // {
+    //     ROS_WARN("No 'process' argument provided!");
+    // }
 
-    /** */
-    
-    /** Store value if argument passed*/
-    if (argc > 1)
+    /** Initialise process*/
+    double process;
+    nh.getParam("process",process);
+
+    std::cout << "process = " << process << std::endl;
+
+    /** Initialise variables*/
+    double yaw;
+    double x, y, z, w;
+
+    /** Process 0, convert Yaw to Quaternion only*/
+    if (process > 0 - 1e-2 && process < 0 + 1e-2)
     {
-        tb = argv[1];
-        ROS_INFO("The 'tb' argument is %s", tb.c_str());
+        nh.getParam("yaw", yaw);
+        /** Convert yaw to quaternion*/
+        std::tuple<double, double, double, double> q = yawToQuaternion(yaw);
+        ROS_INFO("Converted Yaw to Quaternion: [x: %f, y: %f, z: %f, w: %f]",
+                 std::get<0>(q), std::get<1>(q),
+                 std::get<2>(q), std::get<3>(q));
+        
+        /** Set params*/
+        nh.setParam("converted_x",std::get<0>(q));
+        nh.setParam("converted_y",std::get<1>(q));
+        nh.setParam("converted_z",std::get<2>(q));
+        nh.setParam("converted_w",std::get<3>(q));
     }
-    /** Otherwise continue with no namespace*/
-    else
+    /** Process 1, convert Quaternion to Yaw only*/
+    else if (process > 1 - 1e-2 && process < 1 + 1e-2)
     {
-        std::cout << "No 'tb' argument provided!" << std::endl;
+        nh.getParam("quaternion_x", x);
+        nh.getParam("quaternion_y", y);
+        nh.getParam("quaternion_z", z);
+        nh.getParam("quaternion_w", w);
+        /** Convert quaternion to yaw*/
+        double yaw_q = quaternionToYaw(x, y, z, w);
+        ROS_INFO("Converted Quaternion to Yaw: %f", yaw_q);
+
+        /** Set param*/
+        nh.setParam("converted_yaw",yaw_q);
+    }
+    /** Process 2, convert both*/
+    else if (process > 2 - 1e-2 && process < 2 + 1e-2)
+    {
+        nh.getParam("yaw", yaw);
+        /** Convert yaw to quaternion*/
+        std::tuple<double, double, double, double> q = yawToQuaternion(yaw);
+        ROS_INFO("Converted Yaw to Quaternion: [x: %f, y: %f, z: %f, w: %f]",
+                 std::get<0>(q), std::get<1>(q),
+                 std::get<2>(q), std::get<3>(q));
+        
+        nh.getParam("quaternion_x", x);
+        nh.getParam("quaternion_y", y);
+        nh.getParam("quaternion_z", z);
+        nh.getParam("quaternion_w", w);
+        /** Convert quaternion to yaw*/
+        double yaw_q = quaternionToYaw(x, y, z, w);
+        ROS_INFO("Converted Quaternion to Yaw: %f", yaw_q);
+
+        /** Set params*/
+        nh.setParam("converted_x",std::get<0>(q));
+        nh.setParam("converted_y",std::get<1>(q));
+        nh.setParam("converted_z",std::get<2>(q));
+        nh.setParam("converted_w",std::get<3>(q));
+        nh.setParam("converted_yaw",yaw_q);
+        
     }
 
-    double yaw = ...
-
-        Quaternion q = yawToQuaternion(yaw);
-    std::cout << "Quaternion: (" << q.x << ", " << q.y << ", " << q.z << ", " << q.w << ")" << std::endl;
+    ros::spin();
 
     return 0;
 }
