f0008c4b7b8f24a54bc98b3801e8e75cc17b24a3
john <john.e.hunter-1@student.uts.edu.au>
2024-06-01 23:18:36 +1000

    multi_nav_updates

--- multi_nav/src/global_position2.cpp
+++ multi_nav/src/global_position2.cpp
@@ -10,8 +10,8 @@ public:
     GetGlobalPosition2()
     {
         ros::NodeHandle nh;
-        nh.getParam("tb3_0", tb3_0);
-        nh.getParam("tb3_1", tb3_1);
+        nh.getParam("/global_position2/tb0", tb3_0);
+        nh.getParam("/global_position2/tb1", tb3_1);
 
         pose_marker_pub_1 = nh.advertise<visualization_msgs::Marker>("/" + tb3_0 + "/marker/position", 10);
         pose_marker_pub_2 = nh.advertise<visualization_msgs::Marker>("/" + tb3_1 + "/marker/position", 10);
@@ -106,8 +106,8 @@ private:
     {
         try
         {
-            listener.waitForTransform("/map", target_frame, ros::Time(0), ros::Duration(3.0));
-            listener.lookupTransform("/map", target_frame, ros::Time(0), transform);
+            listener.waitForTransform("map", target_frame, ros::Time(0), ros::Duration(3.0));
+            listener.lookupTransform("map", target_frame, ros::Time(0), transform);
             return true;
         }
         catch (tf::TransformException &ex)
--- multi_nav/src/global_position3.cpp
+++ multi_nav/src/global_position3.cpp
@@ -10,9 +10,9 @@ public:
     GetGlobalPosition3()
     {
         ros::NodeHandle nh;
-        nh.getParam("tb3_0", tb3_0);
-        nh.getParam("tb3_1", tb3_1);
-        nh.getParam("tb3_2", tb3_2);
+        nh.getParam("/global_position3/tb0", tb3_0);
+        nh.getParam("/global_position3/tb1", tb3_1);
+        nh.getParam("/global_position3/tb2", tb3_2);
 
         pose_marker_pub_1 = nh.advertise<visualization_msgs::Marker>("/" + tb3_0 + "/marker/position", 10);
         pose_marker_pub_2 = nh.advertise<visualization_msgs::Marker>("/" + tb3_1 + "/marker/position", 10);
