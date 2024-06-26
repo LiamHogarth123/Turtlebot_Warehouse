ad15b4f6ac0387de554a6fdc652587f78e2c6e28
LiamHogarth123 <liam.hogarth@student.uts.edu.au>
2024-06-27 00:55:57 +1000

    removing broken final veriosn

--- final_version/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liam/git/Turtlebot_Warehouse/final_version
-# Build directory: /home/liam/git/Turtlebot_Warehouse/final_version/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
